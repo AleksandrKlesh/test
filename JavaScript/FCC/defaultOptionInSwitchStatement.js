4929aa04b9a275a3d07d3355eb23088a572fe11d
AleksandrKlesh <63539785+AleksandrKlesh@users.noreply.github.com>
2021-12-07 22:43:46 +0300

    added some javascript

---
+++ JavaScript/FCC/defaultOptionInSwitchStatement.js
@@ -0,0 +1,24 @@
+// node defaultOptionInSwitchStatement.js
+
+function switchOfStuff(val) {
+  var answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  return answer;
+}
+
+console.log(switchOfStuff("a"));
+console.log(switchOfStuff(2));
+console.log(switchOfStuff("c"));
---
+++ JavaScript/FCC/multiSwitchStatements.js
@@ -0,0 +1,27 @@
+// node multiSwitchStatements.js
+
+function sequeatialSizes(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+console.log(sequeatialSizes(1));
+console.log(sequeatialSizes(5));
+console.log(sequeatialSizes(9));
