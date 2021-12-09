ecb435ca686b1cde325387f631e010ecdbeb3ebe
AleksandrKlesh <63539785+AleksandrKlesh@users.noreply.github.com>
2021-12-09 21:28:52 +0300

    added some JS

---
+++ JavaScript/FCC/nestedArrays.js
@@ -0,0 +1,16 @@
+// node nestedArrays.js
+
+var myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+var secondTree = myPlants[1].list[1];
+
+console.log(secondTree);
---
+++ JavaScript/FCC/recordCollection.js
@@ -0,0 +1,1 @@
+// node recordCollection.js
