1270286df37c207369aa971775253a7ccc4ab24f
AleksandrKlesh <63539785+AleksandrKlesh@users.noreply.github.com>
2021-12-09 21:04:12 +0300

    added some JS

---
+++ JavaScript/FCC/complexObj.js
@@ -0,0 +1,19 @@
+// node complexObj.js
+
+var myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  }, // add record
+  {
+    artist: "Beau Carnes",
+    title: "Cereal Man",
+    release_year: 2003,
+    formats: ["YouTube video"],
+  },
+];
+
+console.log(myMusic);
---
+++ JavaScript/FCC/testObjForProper.js
@@ -0,0 +1,18 @@
+// node testObjForProper.js
+
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh",
+};
+
+function checkObj(checkProp) {
+  if (myObj.hasOwnProperty(checkProp)) {
+    return myObj[checkProp];
+  } else {
+    return "Not found";
+  }
+}
+
+console.log(checkObj("gift"));
+console.log(checkObj("hello"));
