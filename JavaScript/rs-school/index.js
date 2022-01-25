46f796effb310ba2a9037397d06b63f48c46fc3d
AleksandrKlesh <63539785+AleksandrKlesh@users.noreply.github.com>
2022-01-25 23:34:06 +0300

    study arrays, objects, functions

---
+++ JavaScript/rs-school/index.js
@@ -0,0 +1,74 @@
+// console.log("Hello");
+// console.log(5 + 8);
+
+// let a = 5;
+// const b = 6;
+// const obj {
+//   a: 5,
+//   b: 'sss',
+// };
+
+// console.log('a: ', a);
+
+// console.log(Number("5"));
+// console.log(NaN);
+// console.log(Number("gsjdogfd"));
+// console.log(isNaN(NaN));
+// console.log(null);
+// console.log(undefined);
+// let s;
+// console.log(s);
+
+//Objects
+
+const obj = {
+  a: 5,
+  hello: "hello",
+};
+
+const obj0 = new Object();
+
+// console.log(obj.a, obj.hello);
+
+// const arr = new Array(5).fill(6);
+// const arr0 = ["s", { a: 4 }, 7];
+
+// const arr = ["k", { a: 4 }, 7];
+
+// console.log(arr);
+
+// const map = new Map();
+// console.log(map);
+
+// const map0 = new Map([
+//   ["a", 6],
+//   ["key", "value"],
+// ]);
+
+// console.log(map0.keys());
+// console.log(map0.get("a"));
+// console.log(map0.get("key"));
+
+// const set = new Set([1, 1, 1, 1, 3, "d"]);
+
+// console.log(set);
+
+// const date = new Date();
+
+// console.log(date.getFullYear());
+
+// function
+
+function func0() {
+  console.log("func0");
+  return "664942"; // or undefined without return
+}
+
+func0();
+
+function func1() {
+  console.log("func0 dsgdgd:");
+  return "664942"; // or undefined without return
+}
+
+func1();
