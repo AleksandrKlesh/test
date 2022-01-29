08aec05c9914a3f84a0c17a58bac25def0df35ee
AleksandrKlesh <63539785+AleksandrKlesh@users.noreply.github.com>
2022-01-30 00:29:36 +0300

    study functions

---
+++ JavaScript/rs-school/JSFunctiions/functions.js
@@ -0,0 +1,189 @@
+// const str = "Hello World";
+// const num = 10;
+
+// const add = function (a, b) {
+//   const str = "Hello All World";
+//   console.log(str);
+//   return a + b;
+// };
+
+// function reduce(a, b) {
+//   return a - b;
+// }
+
+// const multiple = (a,b) => {
+//   return a*b;
+// }
+
+// console.log(reduce(20, 10));
+// console.log(add(10, 20));
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(add);
+
+// const string = "10";
+// const number = 10;
+// const boolean = true;
+// const object = {};
+// const nullVar = null;
+// const undefinedVar = undefined;
+
+// const result = boolean + object;
+// console.log(typeof result, result);
+
+// const result1 = string + number;
+// console.log(typeof result1);
+// console.log(result1);
+
+// const result2 = boolean + string;
+// console.log(typeof result2, result2);
+
+// const result3 = boolean + number;
+// console.log(typeof result3, result3);
+
+// console.log(Number(""));
+// console.log(Number("string"));
+// console.log(Number({}));
+// console.log(Number(nullVar));
+// console.log(Number(undefinedVar));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number([3, 2]));
+
+// console.log(Number(23));
+// console.log(Number(-23));
+
+// console.log(String(""));
+// console.log(String("string"));
+// console.log(String({}));
+// console.log(String(nullVar));
+// console.log(String(undefinedVar));
+// console.log(String(true));
+// console.log(String(false));
+// console.log(String([3, 2]));
+
+// console.log(String(23));
+// console.log(String(-23));
+
+// const str = "10";
+// const num = 10;
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// if (str == num) {
+//   console.log("was true");
+// }
+
+// console.log(add());
+
+// const str = { b: 2 };
+// const num = { a: 1 };
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// if (str == num) {
+//   console.log("was true");
+// }
+
+// console.log(add());
+
+// const str = "10";
+// const num = 10;
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// if (str === num) {
+//   console.log("was true");
+// }
+
+// console.log(add());
+
+// const bool = false;
+// const num = 0;
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// if (bool === Boolean(num)) {
+//   console.log("was true");
+// }
+
+// console.log(add());
+
+// const bool = false;
+// const num = 1;
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// if (Boolean(num)) {
+//   console.log("was true");
+// }
+
+// const comparition = (a) => {
+//   if (a > 0) {
+//     return 'more than zero'
+//   } else {
+//     return 'less or equal than 0'
+//   }
+// }
+
+// const result = comparition(20)
+
+// console.log(result);
+
+// const comparition = (a) => {
+//   if (a > 0) {
+//     return 'more than zero'
+//   } else {
+//     return 'less or equal than 0'
+//   }
+// }
+
+// const comparitionAdv = (a) => {
+//   return a > 0 ? "more than zero" : "less or equal than 0";
+// };
+
+// const result = comparition(20);
+// const result = comparitionAdv(20);
+// console.log(result);
+
+const comparition = (a) => {
+  if (a > 0) {
+    return "more than zero";
+  } else if (a === 0) {
+    return "eaual 0";
+  } else {
+    return "less than 0";
+  }
+};
+
+// const result = comparition(-5);
+// console.log(result);
+
+// comparitionSwitch = (a) => {
+//   switch (a) {
+//     case 20:
+//       console.log("more than 0");
+//       break;
+//     case 0:
+//       return "equal 0";
+//     default:
+//       return "less than zero";
+//   }
+// };
+
+// const result = comparitionSwitch(20);
+
+//            for
---
+++ JavaScript/rs-school/JSbasics/index.js
@@ -0,0 +1,302 @@
+// console.log("Hello");
+// console.log(5 + 8);
+
+// let a = 5;
+// const b = 6;
+// const obj {
+//   a: 5,
+//   b: 'sss',
+// };
+
+// console.log('a: ', a);
+
+// console.log(Number("5"));
+// console.log(NaN);
+// console.log(Number("gsjdogfd"));
+// console.log(isNaN(NaN));
+// console.log(null);
+// console.log(undefined);
+// let s;
+// console.log(s);
+
+//Objects
+
+// const obj = {
+//   a: 5,
+//   hello: "hello",
+// };
+
+// const obj0 = new Object();
+
+// console.log(obj.a, obj.hello);
+
+//          array
+
+// const arr = new Array(5).fill(6);
+// const arr0 = ["s", { a: 4 }, 7];
+
+// const arr = ["k", { a: 4 }, 7];
+
+// console.log(arr);
+
+//           map
+
+// const map = new Map();
+// console.log(map);
+
+// const map0 = new Map([
+//   ["a", 6],
+//   ["key", "value"],
+// ]);
+
+// console.log(map0.keys());
+// console.log(map0.get("a"));
+// console.log(map0.get("key"));
+
+// const set = new Set([1, 1, 1, 1, 3, "d"]);
+
+// console.log(set);
+
+// const date = new Date();
+
+// console.log(date.getFullYear());
+
+//          function
+
+// function func0() {
+//   console.log("func0");
+//   return "664942"; // or undefined without return
+// }
+
+// func0();
+
+// function func1() {
+//   console.log("func0 dsgdgd:");
+//   return "664942"; // or undefined without return
+// }
+
+// func1();
+
+//             Strings
+
+// function oldStyleFunc(a, b, c) {
+//   return a + b + c;
+// }
+
+// const arrowFunc = (d, e) => {
+//   // return `$(d) + $(e)`;
+//   return d + e;
+// };
+
+// const str = `str`;
+
+// console.log(arrowFunc("John", " Smith"));
+
+// function oldStyleFunc(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log("- How are you doing?\n- I'm fine");
+
+//          Boolean
+
+// console.log(true);
+// console.log(false);
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 === 5);
+// console.log(5 == "5");
+// console.log(5 !== "5");
+// console.log(5 != "5");
+
+// console.log(5 || 6);
+// console.log(undefined || 6);
+// console.log(undefined || "5555");
+// console.log(null || "5555");
+
+// let a = undefined;
+
+// let b = null;
+
+// console.log(a || b);
+// console.log(b || a);
+// console.log(b || 5);
+
+//          || &&
+
+// console.log(false && true);
+
+// let isShow = true;
+
+// console.log(isShow);
+
+// console.log(isShow && "comp");
+
+// console.log((isShow = false));
+
+// console.log(isShow && "comp");
+
+//            ??
+
+// console.log(null ?? 5);
+
+// console.log(undefined ?? 5);
+
+// console.log(0 || 5);
+
+// console.log(0 ?? 5);
+
+//         !6
+
+// console.log(!6);
+
+// console.log(!true);
+// console.log(!false);
+// console.log(!!5);
+// console.log(!!"fffff");
+
+//        function
+
+// function func(arg0, argFunc) {
+//   return argFunc(arg0);
+// }
+
+// console.log(func(5, (x) => x + 1));
+
+// const arrF = (arg0, argFunc) => {
+//   return argFunc(arg0);
+// };
+
+// console.log(arrF(5, (x) => x + 15));
+
+//           Math
+
+// const max = Math.abs(-4);
+// console.log(max);
+
+//           Properties and methods
+
+// const arr = [1, 2, 3, "4"];
+// const arr1 = arr.push("5");
+
+// console.log(arr1, arr);
+
+// const arr = [1, 2, 3, "4"];
+// const arr1 = arr.join(", fsdffs");
+
+// console.log(arr, arr1);
+
+// const arr = [1, 2, 3, "4"];
+// const mapped = arr.map((item, index) => `${item} - ${index}`);
+
+// console.log(arr, mapped.length);
+
+//              if
+
+// function plur(word, quantity) {
+//   if (quantity > 1) {
+//     return `${quantity} ${word}s`;
+//   }
+//   return `${quantity} ${word}`;
+// }
+
+// console.log(plur("user", 1));
+// console.log(plur("user", 6));
+
+//   if (document.validated === "fully") {
+//     document.status = "validated";
+//   } else if (document.validated === "half") {
+//     document.status === "half validated";
+//   } else {
+//     document.status = "not validated";
+//   }
+// }
+
+// function setStatus(document) {
+//   switch (document.validated) {
+//     case 'fully':
+//       document.status = 'validated'
+//       break;
+
+//       case 'half':
+//       document.status = 'half validated'
+//       break;
+
+//     default:
+//       document.status === "not validated";
+//       break;
+//   }
+
+//   const doc0 = {
+//     validated: "fully",
+//   };
+
+//   const result = setStatus(doc0);
+
+// console.log(result);
+
+//          Ternary operator
+
+// const a = true ? "000" : "6666";
+// const b = false ? "000" : "6666";
+
+// console.log(a, b);
+
+// const isShow = 6 > 8;
+// const a = isShow ? "000" : "6666";
+
+// console.log(a, isShow);
+
+//               While
+
+// const max = 5;
+// let i = 0;
+
+// while (i < max) {
+//   i++;
+//   console.log("i: ", i);
+// }
+
+// const max = 5;
+// let i = 0;
+
+// function a() {
+//   while (i < max) {
+//     i++;
+//     console.log("i: ", i);
+//     if (i === 3) {
+//       return;
+//     }
+//   }
+// }
+
+// a();
+
+//                For
+
+// const f = () => {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// };
+
+// f();
+
+//               Reduce
+
+// const arr = ["My", "name", "is", "Santa"];
+
+// const reducer = (prev, curr) => `${prev} ${curr}`;
+
+// console.log(arr.reduce(reducer, ""));
+
+// const arr = ["My", "name", "is", "Santa"];
+
+// const reducer = (prev, curr) => {
+//   console.log("/n");
+//   console.log(curr);
+//   console.log(prev);
+//   return `${prev} ${curr}`;
+// };
+
+// console.log(arr.reduce(reducer, ""));
